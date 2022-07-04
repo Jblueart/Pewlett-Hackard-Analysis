8a21e17d24298ebbe372165f45d6c29f8034f419
Jblueart <104408782+Jblueart@users.noreply.github.com>
2022-07-04 11:20:32 -0600

    Add files via upload

--- Pewlett_Hackard_queries.sql
+++ Pewlett_Hackard_queries.sql
@@ -36,3 +36,208 @@ select * titles
  select * from departments; --Remember to capslock commands & lowercase variables SELECT * FROM employees --Create table titles CREATE TABLE "titles"( emp_no INT NOT NULL, title VARCHAR not null, from_date DATE, to_date DATE, Primary Key (emp_no), Unique (emp_no) );
  select * from departments; --Remember to capslock commands & lowercase variables SELECT * FROM employees --Create table titles CREATE TABLE titles( emp_no INT NOT NULL, title VARCHAR not null, from_date DATE, to_date DATE, Primary Key (emp_no), Unique (emp_no) );
  select * from departments; --Remember to capslock commands & lowercase variables SELECT * FROM employees --Create table titles CREATE TABLE titles( emp_no INT NOT NULL, title VARCHAR not null, from_date DATE, to_date DATE, Primary Key (emp_no), Unique (emp_no) );
+
+
+
+
+--Joining departments & dept_manager tables
+SELECT departments.dept_name, 
+	dept_manager.emp_no,
+	dept_manager.from_date,
+	dept_manager.to_date
+	
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+--Aliasy Version
+
+SELECT d.dept_name, 
+	dm.emp_no, 
+	dm.from_date,
+	dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+
+
+
+--Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+	retirement_info.first_name,
+retirement_info.last_name,
+	dept_emp.to_date
+	--assign left table with from
+FROM retirement_info	
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+
+---aliasy version
+
+SELECT ri.emp_no, 
+	ri.first_name, 
+ri.last_name, 
+	de.to_date
+	
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+
+
+-- SAME RESULT! 
+
+
+
+-- Use left join for Retirement Info * dept_emp tables
+Select ri.emp_no, 
+	ri.first_name, 
+	ri.last_name, 
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+----
+
+--LIST ONE EMPLOYEE Information
+
+SELECT emp_no, 
+	first_name, 
+	last_name,
+	gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--add salary columns and to_date
+
+DROP TABLE emp_info;
+
+SELECT e.emp_no,
+e.last_name,
+	e.gender,
+s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+	
+	
+
+
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+
+SELECT * FROM manager_info;
+
+
+
+--Department info
+
+SELECT ce.emp_no, 
+ce.first_name, 
+ce.last_name, 
+d.dept_name
+INTO dept_info as di
+FROM current_emp as ce
+INNER JOIN  dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (di.dept_no = d.dept_no);
+
+
+-----------------------------
+--Cue_Retirement
+-- Module 7
+DROP TABLE cue_retirement;
+
+SELECT e.emp_no, 
+e.first_name, 
+e.last_name,
+e.birth_date,
+e.hire_date,
+ti.title, 
+ti.from_date,
+ti.to_date
+INTO cue_retirement
+From employees as e 
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (ti.to_date ='9999-01-01');
+	
+	
+	--CURRENT_retirement
+SELEct DISTINCT emp_no
+first_name, 
+last_name, 
+birth_date,
+title, 
+from_date, 
+to_date
+INTO current_retirement
+From cue_retirement
+WHERE (from_date >= hire_date)
+	AND(to_date = '9999-01-01');
+
+--Retiring_titles csv
+
+
+SELECT title, count(*) AS "Number of Employees"
+INTO retiring_titles
+FROM current_retirement
+GROUP BY title
+ORDER BY "Number of Employees" DESC;
+
+
+
+--Mentorship Eligibility
+
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date, 
+titles.title,
+titles.to_date
+INTO mentorship_eligibility
+FROM employees as e 
+--DISTINCT ON e.emp_no
+LEFT JOIN titles ON e.emp_no = titles.emp_no
+LEFT JOIN emp_info
+ON e.emp_no = emp_info.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') 
+
+AND (titles.to_date = '9999-01-01')
+ORDER BY birth_date DESC;
+
+
+SELECT * FROM mentorship_eligibility;
+
--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # Pewlett-Hackard-Analysis
+
+
+Examining the employees and departments who have employees who will retire soon. 
+This is helpful because we can plan and predict how many new employees we will need to replace our older workforce. 
+The employees who plan to retire will have enough time to mentor some of the employees, candidates are listed on the mentor_eligible.csv sheet. 
+
+## Prepping for the 'Silver Tsunami' 
+
+ Since 33,117 people are eligible for retirement, we'll definitly need to increase our recruitment strategies as we only have 1548 individuals eligible for mentorship under the current program parameters. 
+
+
+### Resources
+Data Tools: PostgreSQL, pgAdmin 4
+
+
+
+SQL language invites a robust analysis of the information. We're able to transform the data into actionable databases with valuable information. 
+Because we're able to combine tables, we can create new reference sheets that allow for a deeper understanding. 
+
+We've been unpacking Tables, and starting to understand the headers, as well as primary & foreign keys. 
+We've identified the need for the primary keys to be Unique to each entry to maintain data integrity. 
+
+
+### Entity Relationship Diagram
+ERDs allow us to document and exdplore existing databases with a visual represtation. It helps to analyze and see the relationships between the various tables. 
+Since ERDs show us how the data connects, they make it easier to interpret and manipulate the information into more clear databases. 
+
+
+
+
+## Unique titles per employees eligible for retirment.
+
+33117 entries found on Uniquetitles.csv
+
+### Eligible Staff by title: 
+
+retiring_titles.csv
+
+## Number of employees retiring by title
+Senior Engineer: 13651
+Senior Staff: 12872
+Engineer: 2711
+Staff: 2022
+Technique Leader: 1609
+Assistant Engineer: 251
+Manager: 2
+
+## Mentorship Eligibility
+
+1548 individuals total
+
+### Breakdown by title of Mentorees
+
+Senior Staff: 569
+Senior Engineer: 529
+Engineer: 190
+Staff: 155
+Technique Leader: 77
+Assistant Engineer: 29
+
+#### Many Senior level positions soon to open up
+## Opportunity Knocks! 
+
+
